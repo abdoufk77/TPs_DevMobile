54decd3826779802384b05f63e41b94ebe5ec4bf
abdoufk77 <ab.foukahy@gmail.com>
2025-12-24 20:58:27 +0100

    code principal

--- App.js
+++ App.js
@@ -1,9 +1,26 @@
-import { Text, View } from "react-native";
+import { Image, StyleSheet, View } from "react-native";
 
 export default function App() {
   return (
-    <View>
-      <Text>hello React Native</Text>
+    <View style={styles.container}>
+      <Image
+        source={require("./assets/emsi.png")}
+        style={styles.logo}
+        resizeMode="contain"
+      />
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f6f7fb",
+  },
+  logo: {
+    width: 200,
+    height: 200,
+  },
+});
