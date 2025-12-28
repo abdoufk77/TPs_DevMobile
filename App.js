e1b02c0cd8ead571b70904c435b073aa1c9dfe5f
abdoufk77 <ab.foukahy@gmail.com>
2025-12-28 22:24:03 +0100

    Connexion à une API distante avec axios

--- App.js
+++ App.js
@@ -1,9 +1,33 @@
-import { Text, View } from "react-native";
-
-export default function App() {
+import { useContext } from "react";
+import { View, StyleSheet } from "react-native";
+import { ThemeProvider, ThemeContext } from "./context/ThemeContext";
+import TodoListFetchScreen from "./screens/TodoListFetchScreen";
+function MainApp() {
+  const { theme } = useContext(ThemeContext);
   return (
-    <View>
-      <Text>hello React Native</Text>
+    <View
+      style={[styles.container, theme === "dark" ? styles.dark : styles.light]}
+    >
+      <TodoListFetchScreen />
     </View>
   );
 }
+export default function App() {
+  return (
+    <ThemeProvider>
+      <MainApp />
+    </ThemeProvider>
+  );
+}
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    paddingTop: 40,
+  },
+  light: {
+    backgroundColor: "#ffffff",
+  },
+  dark: {
+    backgroundColor: "#121212",
+  },
+});
